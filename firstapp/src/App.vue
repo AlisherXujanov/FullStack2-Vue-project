abed4d39a05c04b84914f177e1b2a9c4a95101ea
Alisher <alisherxujanov163@gmail.com>
2023-05-10 12:02:02 +0500

    10-th May

--- firstapp/src/App.vue
+++ firstapp/src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div>
     <h1>App</h1>
-    <HelloWorldVue message="This is first message" />
-    <HelloWorldVue message="This is second message" />
-    <HelloWorldVue message="This is third message" />
+    <OwnerCard 
+      v-for="(user, index) in users"
+      :key="user.name"
+      :title="'This is ' + (parseInt(index)+1) + ' title'"
+      :owner="user"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorldVue from './components/HelloWorld.vue'
+import OwnerCard from './components/OwnerCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorldVue,
+    OwnerCard
   },
   data() {
     return {
-
+      users: [
+        { name: "John", date: '2022-10-20', views: 512 },
+        { name: "John 2", date: '2022-10-20', views: 132 },
+        { name: "John 3", date: '2022-10-20', views: 123 },
+        { name: "John 4", date: '2022-10-20', views: 222 },
+        { name: "John 5", date: '2022-10-20', views: 152 },
+      ]
     }
   },
   methods: {},
---
+++ firstapp/src/components/OwnerCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="img">Image</div>
+    <b>{{ title }}</b>
+    <p class="subtitle">{{ subtitle }}</p>
+    <div class="footer">
+      <div class="left">
+        <span class="owner"
+          >🎅
+          <!-- owner.img -->
+        </span>
+        <small>{{ owner.name }}</small>
+        <small>{{ owner.date }}</small>
+      </div>
+
+      <div class="right">
+        <p>👁 {{ owner.views }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OwnerCard",
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: "Default title",
+      validate: (value) => value.length > 5,
+    },
+    subtitle: {
+      type: String,
+      required: false,
+      default: "Default subtitle",
+      validate: (value) => value.length > 5,
+    },
+    owner: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
