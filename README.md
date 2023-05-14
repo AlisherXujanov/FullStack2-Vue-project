7f22976fcd26773f9b85c661288f906fb881961a
Alisher <alisherxujanov163@gmail.com>
2023-05-14 11:54:25 +0500

    installed sass

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 2. cd   firstapp
 3. npm install
 4. npm run serve
+
+## Installing SASS
+1. npm install -D sass-loader@^10 sass 
+2. USE ->  **<style lang='scss' coped>**
+3.  module.exports = {
+        css: {
+            loaderOptions: {
+            sass: { data: `@import "@/styles/_variables.scss";` }
+            }
+        }
+    };
+    **Use this inside vue.config.js file**
--- firstapp/src/components/BannerStar.vue
+++ firstapp/src/components/BannerStar.vue
@@ -1,65 +1,68 @@
 <template>
   <div class="banner-main">
     <div class="left">
-        <img 
-            src="../assets/icons/star.png" 
-            alt="Star image" 
-            width="200px" 
-            height="200px"
-        >
+      <img
+        src="../assets/icons/star.png"
+        alt="Star image"
+        width="200px"
+        height="200px"
+      />
     </div>
     <div class="right">
-        <h4>{{ title }}</h4>
-        <h2>{{ text }}</h2>
-        <button>
-            Перейти в кошелёк
-        </button>
+      <h4>{{ title }}</h4>
+      <h2>{{ text }}</h2>
+      <button>Перейти в кошелёк</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    props: [ "title", "text" ],
-    data() {
-        return {}
-    }
-}
+  props: ["title", "text"],
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style lang="css" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
-.banner-main {
-    width: 85%;
-    height: 450px;
-    margin: 0 auto;
-    background: linear-gradient(to right top, #4B1613, #FAC704);
-    border-radius: 25px;
-    font-size: 1.5em;
-    color: snow;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    padding: 100px;
-    font-family: 'Roboto', sans-serif;;
-}
-h2 { margin: 50px 0; }
-h4 { 
-    font-size: 1.2em; 
-    font-weight: 500;
-}
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
+@import "../assets/css/variables.scss";
 
-.banner-main .right {
+.banner-main {
+  width: 85%;
+  height: 450px;
+  margin: 0 auto;
+  background: linear-gradient(to right top, #4b1613, $yellow);
+  border-radius: 25px;
+  font-size: 1.5em;
+  color: snow;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding: 100px;
+  font-family: "Roboto", sans-serif;
+
+  .right {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: end;
     text-align: right;
-}
-button {
+  }
+
+  button {
     border: snow solid 3px;
     padding: 15px 35px;
     color: snow;
-    background-color: #F8F8F810;
+    background-color: rgb($yellow, 0.1);
     font-size: 1em;
+  }
+}
+h2 {
+  margin: 50px 0;
+}
+h4 {
+  font-size: 1.2em;
+  font-weight: 500;
 }
 </style>>
