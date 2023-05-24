70f98ed858c9f98e5f140256a76fa0b3d1c91b1b
Alisher <alisherxujanov163@gmail.com>
2023-05-24 23:13:10 +0500

    added AddCard component

---
+++ firstapp/src/components/Card/AddCard.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="add-card-section">
+    <h1>Create Card</h1>
+    <form action="">
+      <div>
+        <label for="title">Title</label>
+        <input type="text" id="title" v-model="title" />
+      </div>
+      <div>
+        <label for="content">Content</label>
+        <textarea name="content" id="content" cols="30" rows="5" v-model="content"></textarea>
+      </div>
+      <div>
+        <label for="favorite">Favorite</label>
+        <input type="checkbox" id="favorite" v-model="favorite" />
+      </div>
+      <div>
+        <label for="date">Date</label>
+        <input type="date" id="date" v-model="date" />
+      </div>
+      <button type="button" @click="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    newCardId: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: '',
+      content: '',
+      favorite: false,
+      date: ''
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('submit', {
+        // id: this.newCardId,
+        id: 5,  // WE ARE HARDCODING THE ID TO 5 for pictures
+        title: this.title,
+        content: this.content,
+        favorite: this.favorite,
+        date: this.date
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#add-card-section {
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+
+  h1 { font-size: 3em; }
+
+  form {
+    width: 100%;
+
+
+    input:not([type='checkbox']), textarea {
+      width: 50%;
+      padding: 10px 20px;
+      display: block;
+      outline: none;
+      border-radius: 5px;
+      margin-bottom: 20px;
+      transition: 0.3s;
+      font-size: 1.2em;
+
+      &:focus:not([type='checkbox']) { outline: 5px solid rgb(0, 255, 255, 0.5); }
+    }
+    input[type='checkbox'] { margin: 20px; width: 20px; height: 20px }
+    label {
+      width: 50%;
+      margin: 0 auto;
+      font-size: 1.2em;
+      font-weight: bold;
+      text-align: left;
+    }
+
+    button {
+      margin-top: 20px;
+      font-weight: bold;
+      padding: 10px 20px;
+      border: none;
+      border-radius: 5px;
+      background-color: #28a745;
+      color: #fff;
+      cursor: pointer;
+
+      &:hover { 
+        background-color: #218838;  
+        outline: 5px solid rgb(0, 150, 0, 0.5); 
+      }
+      &:active { background-color: #1e7e34; }
+    }
+  }
+}
+</style>
--- firstapp/src/views/CardsView.vue
+++ firstapp/src/views/CardsView.vue
@@ -1,36 +1,60 @@
 <template>
   <div>
-    <div class="filter-section">
-      <button @click="filterBy('by__date')" :class="filter.by__date ? 'active' : ''">By Date</button>
-      <button @click="filterBy('by__favorite')" :class="filter.by__favorite ? 'active' : ''">By Favorite</button>
-      <button @click="filterBy('default')" :class="filter.default ? 'active' : ''">All Cards</button>
+    <div class="section-toggle-btns">
+      <button @click="toggleSection('addCard')" :class="section.addCard ? 'active' : ''">
+        Add Card
+      </button>
+      <button @click="toggleSection('cards')" :class="section.cards ? 'active' : ''">Cards</button>
     </div>
 
-    <div class="cards">
-      <Card
-        v-for="card in filteredCards"
-        :key="card.id"
-        :card="card"
-        :imgId="card.id"
-        @toggle-favorite="toggleFavorite"
-        v-slot:default
-      >
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, sit eaque quod voluptate
-          ipsam adipisci et minus deserunt sequi reprehenderit neque quisquam corrupti laudantium
-          corporis rem fuga beatae magnam ad.
-        </p>
-      </Card>
+    <div class="content">
+      <section v-if="section.addCard">
+        <div>
+          <AddCard @submit="addCard" :new-card-id="cards.length + 1" />
+        </div>
+      </section>
+      <section v-else>
+        <div class="filter-section">
+          <button @click="filterBy('by__date')" :class="filter.by__date ? 'active' : ''">
+            By Date
+          </button>
+          <button @click="filterBy('by__favorite')" :class="filter.by__favorite ? 'active' : ''">
+            By Favorite
+          </button>
+          <button @click="filterBy('default')" :class="filter.default ? 'active' : ''">
+            All Cards
+          </button>
+        </div>
+
+        <div class="cards">
+          <Card
+            v-for="card in filteredCards"
+            :key="card.id"
+            :card="card"
+            :imgId="card.id"
+            @toggle-favorite="toggleFavorite"
+            v-slot:default
+          >
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, sit eaque quod
+              voluptate ipsam adipisci et minus deserunt sequi reprehenderit neque quisquam corrupti
+              laudantium corporis rem fuga beatae magnam ad.
+            </p>
+          </Card>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <script>
 import Card from '../components/Card/index.vue'
+import AddCard from '../components/Card/AddCard.vue'
 
 export default {
   components: {
-    Card
+    Card,
+    AddCard
   },
   data() {
     return {
@@ -81,7 +105,11 @@ export default {
       filter: {
         by__date: false,
         by__favorite: false,
-        default: true,
+        default: true
+      },
+      section: {
+        addCard: false,
+        cards: true
       }
     }
   },
@@ -130,6 +158,31 @@ export default {
         this.filter[key] = false
       }
       this.filter[order] = true
+    },
+    toggleSection(section) {
+      for (const key in this.section) {
+        this.section[key] = false
+      }
+      this.section[section] = true
+    },
+    addCard(card) {
+      if (!card) return
+      if (!card.title || !card.content || !card.date) {
+        this.$notify({
+          title: 'Error',
+          text: 'Please fill all the fields.',
+          type: 'error'
+        })
+        return
+      } else {
+        this.$notify({
+          title: `Created - ${card.title} - card`,
+          text: 'Card has been added successfully.',
+          type: 'success'
+        })
+        this.cards.push(card)
+        this.toggleSection('cards')
+      }
     }
   }
 }
@@ -146,39 +199,56 @@ export default {
   display: flex;
   justify-content: end;
   margin: 20px 0;
+}
+.section-toggle-btns {
+  float: left;
+}
 
-  button {
-    padding: 10px;
-    border-radius: 5px;
-    cursor: pointer;
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    font-weight: bold;
-    border: 1px solid #ccc;
-    background-color: #333;
-    color: snow;
-    transition: 0.3s;
-    margin-left: 5px;
+button {
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  border: 1px solid #ccc;
+  background-color: #333;
+  color: snow;
+  transition: 0.3s;
+  margin-left: 5px;
 
-    &:hover {
-      /* bootstrap success color */
-      background-color: #28a745;
-      border: 1px solid #28a745;
-      outline: 5px solid rgb(0, 120, 0, 0.5);
-    }
-    &.active {
-      background-color: #28a745;
-    }
-    &:active {
-      background-color: #aaa;
-    }
+  &:hover {
+    background-color: #28a745;
+    border: 1px solid #28a745;
+    outline: 5px solid rgb(0, 120, 0, 0.5);
   }
-
-  
+  &.active {
+    background-color: #28a745;
+  }
+  &:active {
+    background-color: #aaa;
+  }
 }
 
 @media (max-width: 700px) {
   .cards {
     grid-template-columns: repeat(2, 1fr);
+  }
+  .section-toggle-btns {
+    display: flex;
+    justify-content: space-between;
+  }
+  button {
+    width: 50px;
+    font-size: 0.8em;
+  }
+}
+@media (max-width: 500px) {
+  .cards {
+    grid-template-columns: 1fr;
+  }
+  button {
+    width: 40px;
+    font-size: 0.6em;
   }
 }
 </style>
